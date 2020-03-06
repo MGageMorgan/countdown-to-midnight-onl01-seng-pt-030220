b37da534e71b343134f3568e121f40ce6f242cfa
Gage Morgan <gage@gages.blog>
2020-03-06 03:06:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,8 @@ end
 def countdown_with_sleep(number)
   while number > 0
     number -= 1
-    puts "#{number} SECOND(S)!".sleep(1)
+    puts "#{number} SECOND(S)!"
+    sleep(1)
   end
 
   puts "HAPPY NEW YEAR!"
